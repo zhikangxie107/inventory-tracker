2b76495013ce9d93e7ee5e5de06401663485e533
zhikangxie107 <zhikangxie107@gmail.com>
2024-08-03 01:39:44 -0400

    implemented drawer section and app bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,48 @@
-import { Typography, Box } from "@mui/material";
-
+"use client";
+import {
+  Typography,
+  Box,
+  Drawer,
+  Toolbar,
+  Divider,
+  AppBar,
+  CssBaseline,
+} from "@mui/material";
 
 export default function Home() {
   return (
-    <Box className="justify-center items-center flex w-screen h-screen">
-      <Typography>Hello</Typography>
+    <Box className="flex">
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className="bg-blue-500 flex"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Typography
+            className="text-white"
+            variant="h6"
+            noWrap
+            component="div"
+          >
+            Inventory Tracker
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        anchor="left"
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: 240,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar />
+      </Drawer>
     </Box>
-  )
+  );
 }
