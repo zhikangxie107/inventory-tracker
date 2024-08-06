f6a6b69084726198ba8888bebd43ca56d1f736b0
zhikangxie107 <zhikangxie107@gmail.com>
2024-08-06 19:31:26 -0400

    Remove .firebase folder from repository

--- .firebase/inventory-management-14a34/hosting/_next/static/chunks/app/dashboard/page-874158ace9fdb92c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[702],{271:function(n,e,u){Promise.resolve().then(u.bind(u,1789))},1789:function(n,e,u){"use strict";u.r(e),u.d(e,{default:function(){return s}});var t=u(7437),r=u(7999);function s(){return(0,t.jsx)(r.Z,{className:"flex"})}}},function(n){n.O(0,[999,971,23,744],function(){return n(n.s=271)}),_N_E=n.O()}]);
